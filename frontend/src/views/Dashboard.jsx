d289da1661ee4ef109c3139ee917619cf0561aeb
darienvr <118293353+darienvr@users.noreply.github.com>
2023-11-16 12:32:35 -0500

    add dashboard user interface

--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useStateContext } from '../contexts/ContextProvider'
+import axiosClient from '../axios-client';
 
 const Dashboard = () => {
+
+  const {user} = useStateContext()
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+
+  const getUsers = () => {
+    axiosClient.get('/users')
+      .then(({data})=>{
+        setUsers(data.data)
+      })
+      .catch(()=>{
+      })
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Dashboard</h1>
+      <div className='dashboard-container'>
+        <div className='dashboard-section'>
+          <h4>Bienvenido</h4>
+          <h2>{user.name}</h2>
+        </div>
+        <div className='dashboard-section'>
+          <h2 className='users-length'>{users.length}</h2>
+          <h4>Users</h4>
+        </div>
+      </div>
+    </div>
   )
 }
 
