7d6fc67a4085537767887d4ef1169f1f03556d28
darienvr <118293353+darienvr@users.noreply.github.com>
2023-11-16 20:14:22 -0500

    add images to the dashboard

--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -1,6 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import { useStateContext } from '../contexts/ContextProvider'
 import axiosClient from '../axios-client';
+import { FaUser } from "react-icons/fa";
+import { BiSolidMessage } from "react-icons/bi";
+import { TbMessages } from "react-icons/tb";
+import { IoEyeSharp } from "react-icons/io5";
+import graficaLineas from '../assets/grafica-lineas.png'
+import graficaBarras from '../assets/grafica-barras.png'
 
 const Dashboard = () => {
 
@@ -25,13 +31,29 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <div className='dashboard-container'>
         <div className='dashboard-section'>
-          <h4>Bienvenido</h4>
-          <h2>{user.name}</h2>
-        </div>
-        <div className='dashboard-section'>
-          <h2 className='users-length'>{users.length}</h2>
+          <FaUser />
+          <h2>{users.length}</h2>
           <h4>Users</h4>
         </div>
+        <div className='dashboard-section'>
+          <BiSolidMessage />
+          <h2>1500</h2>
+          <h4>Messages</h4>
+        </div>
+        <div className='dashboard-section'>
+          <TbMessages fill='white'/>
+          <h2>322</h2>
+          <h4>conversations</h4>
+        </div>
+        <div className='dashboard-section'>
+          <IoEyeSharp />
+          <h2>2410</h2>
+          <h4>Total Views</h4>
+        </div>
+      </div>
+      <div className='dashboard-graph'>
+        <img src={graficaBarras} alt="Grafica de barras del dashboard" />
+        <img src={graficaLineas} alt="Grafica de barras del dashboard" />
       </div>
     </div>
   )
